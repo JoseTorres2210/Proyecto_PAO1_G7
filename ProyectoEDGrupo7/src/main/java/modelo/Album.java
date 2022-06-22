e6f8f78286d603ee95e099e722f23a9fa61e7247
RobertoPatino1 <rapatino@espol.edu.ec>
2022-06-21 22:35:57 -0500

    Se agregaron metodos extra a la clase Album, asi como el atributo de tipo STRING fotoPortada, que permitira al usuario seleccionar una foto para el album, en caso de que no se le asigne una, la foto para ese album sera una foto default

--- ProyectoEDGrupo7/src/main/java/modelo/Album.java
+++ ProyectoEDGrupo7/src/main/java/modelo/Album.java
@@ -7,13 +7,20 @@ public class Album {
     private String nombre;
     private String descripcion;
     private CircularDoublyLinkedList<Foto> fotos;
-    
+    private String fotoPortada;
     
     //Constructor
-    public Album(String nombre, String descripcion, CircularDoublyLinkedList<Foto> fotos){
+    public Album(String nombre, String descripcion, CircularDoublyLinkedList<Foto> fotos, String fotoPortada){
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.fotos = fotos;
+        this.fotoPortada = fotoPortada;
+    }
+    
+    public Album(String nombre, String descripcion, CircularDoublyLinkedList<Foto> fotos){
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.fotos = fotos; 
     }
     
     //Constructor que no recibe la lista de fotos, pero inicializa una lista vacia
@@ -22,6 +29,7 @@ public class Album {
         this.descripcion = descripcion;
         this.fotos = new CircularDoublyLinkedList<>();  //Solo se inicializa la lista de fotos
     }
+    
 
     public String getNombre() {
         return nombre;
@@ -45,6 +53,21 @@ public class Album {
 
     public void setFotos(CircularDoublyLinkedList<Foto> fotos) {
         this.fotos = fotos;
+    }
+
+    public String getFotoPortada() {
+        return fotoPortada;
+    }
+
+    public void setFotoPortada(String fotoPortada) {
+        this.fotoPortada = fotoPortada;
+    }
+    
+    
+    
+    //Metodo para agregar una foto al album
+    public boolean agregarFoto(Foto foto){
+        return this.fotos.addLast(foto);
     }
     
     
